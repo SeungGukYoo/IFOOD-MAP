f92f379ad616aa11408098bf508399ca44acb48c
SeungGukYoo <dbtmd878@gmail.com>
2023-11-23 15:51:27 +0900

    feat: 카카오 맵 컴포넌트 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,10 @@
-import Link from "next/link";
+"use client";
+import Map from "@/components/Map";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul>
-        <li>
-          <Link href="/stores">Stores</Link>
-        </li>
-        <li>
-          <Link href="/stores/new">new page</Link>
-        </li>
-        <li>
-          <Link href="/stores/1">detail page</Link>
-        </li>
-        <li>
-          <Link href="/stores/1/edit">edit page</Link>
-        </li>
-        <li>
-          <Link href="/user/likes">likes page</Link>
-        </li>
-        <li>
-          <Link href="/user/login">login page</Link>
-        </li>
-        <li>
-          <Link href="/user/mypage">my page</Link>
-        </li>
-      </ul>
-    </main>
+    <>
+      <Map />
+    </>
   );
 }
