8fb2a8bcef51b1e3af1ce25144d6efe28b1a98e8
SeungGukYoo <dbtmd878@gmail.com>
2023-11-23 17:48:33 +0900

    feat: 이미지 마커 기능 추가

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,16 +1,42 @@
+import * as stores from "@/data/seoul_store.json";
+import markerHandler from "@/util/markerHandler";
 import Script from "next/script";
 import React from "react";
+
+const DEFAULT_LAT = 37.497625203;
+const DEFAULT_LNG = 127.03088379;
 
 const Map = () => {
   const loadKakaoMap = () => {
     kakao.maps.load(() => {
       const { kakao } = window;
+
       const mapContainer = document.getElementById("map") as HTMLElement;
+
       const mapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        center: new kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
         level: 3,
       };
-      return new kakao.maps.Map(mapContainer, mapOption);
+
+      const map = new kakao.maps.Map(mapContainer, mapOption);
+
+      stores?.["DATA"].forEach((store, idx) => {
+        if (idx > 100) return;
+        let { y_dnts, x_cnts } = store;
+        let imageSrc = "/images/markers/" + markerHandler(store?.bizcnd_code_nm);
+        let imgMarker = new kakao.maps.MarkerImage(imageSrc, new kakao.maps.Size(40, 40), {
+          offset: new kakao.maps.Point(27, 69),
+          alt: "마커 이미지",
+          shape: "poly",
+        });
+        let markerPos = new kakao.maps.LatLng(Number(y_dnts), Number(x_cnts));
+
+        let marker = new kakao.maps.Marker({
+          position: markerPos,
+          image: imgMarker,
+        });
+        marker.setMap(map);
+      });
     });
   };
   return (
