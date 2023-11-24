d5b020f6eb271ee9bf2e0313d0e71aefb54f2bc2
SeungGukYoo <dbtmd878@gmail.com>
2023-11-24 17:05:10 +0900

    feat: 카카오 코드 훅으로 분리 및 정보 창 구현

---
+++ src/hooks/useMap.tsx
@@ -0,0 +1,71 @@
+import * as stores from "@/data/seoul_store.json";
+import markerHandler from "@/util/markerHandler";
+import React, { useEffect, useState } from "react";
+
+export type StoreType = typeof stores.DATA[number];
+
+const DEFAULT_LAT = 37.497625203;
+const DEFAULT_LNG = 127.03088379;
+
+const useMap = () => {
+  const [map, setMap] = useState<null | kakao.maps.Map>(null);
+  const [currentSotre, setCurrentSotre] = useState<typeof stores.DATA[number] | null>(null);
+
+  const loadKakaoMap = () => {
+    kakao.maps.load(() => {
+      const { kakao } = window;
+      const mapContainer = document.getElementById("map") as HTMLElement;
+
+      const mapOption = {
+        center: new kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
+        level: 3,
+      };
+
+      const map = new kakao.maps.Map(mapContainer, mapOption);
+      setMap(map);
+    });
+  };
+  useEffect(() => {
+    if (map) {
+      stores?.["DATA"].forEach((store, idx) => {
+        if (idx > 100) return;
+        let { y_dnts, x_cnts } = store;
+        let imageSrc = "/images/markers/" + markerHandler(store?.bizcnd_code_nm);
+        let imgMarker = new kakao.maps.MarkerImage(imageSrc, new kakao.maps.Size(40, 40), {
+          offset: new kakao.maps.Point(27, 69),
+          alt: "마커 이미지",
+        });
+        let markerPos = new kakao.maps.LatLng(Number(y_dnts), Number(x_cnts));
+        let marker = new kakao.maps.Marker({
+          position: markerPos,
+          image: imgMarker,
+        });
+        marker.setMap(map);
+        let infoContent = `<div class="infoWindow">${store?.upso_nm}</div>`;
+        let customOverlay = new kakao.maps.CustomOverlay({
+          position: markerPos,
+          content: infoContent,
+          xAnchor: 0.6,
+          yAnchor: 0.91,
+        });
+        kakao.maps.event.addListener(marker, "mouseover", () => {
+          customOverlay.setMap(map);
+        });
+        kakao.maps.event.addListener(marker, "mouseout", () => {
+          customOverlay.setMap(null);
+        });
+        kakao.maps.event.addListener(marker, "click", () => {
+          customOverlay.setMap(map);
+          setCurrentSotre(store);
+        });
+      });
+    }
+  }, [map]);
+  return {
+    currentSotre,
+    loadKakaoMap,
+    setCurrentSotre,
+  };
+};
+
+export default useMap;
