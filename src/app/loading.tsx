0e4655ce3d185e59f72029efcbdfd830272f8879
SeungGukYoo <dbtmd878@gmail.com>
2023-11-27 18:03:49 +0900

    feat: 글로벌 로딩 컴포넌트 추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loading = () => {
+  return <div className="w-full h-[100vh] flex justify-center items-center z-100">Loading...</div>;
+};
+
+export default Loading;
