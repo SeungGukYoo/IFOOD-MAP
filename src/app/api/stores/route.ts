8f1e43509e4876a2994338ef75e7ec5539241616
SeungGukYoo <dbtmd878@gmail.com>
2023-11-27 15:45:31 +0900

    feat: 가게 데이터 api 구현

--- src/app/api/stores/route.ts
+++ src/app/api/stores/route.ts
@@ -1,12 +1,8 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/dist/server/web/spec-extension/response";
 
 type Data = { name: string };
 export async function GET() {
-  const data = {
-    pokemon: {
-      name: "꼬부기",
-    },
-  };
+  const data = await import("@/data/seoul_store.json");
+
   return NextResponse.json({ data });
 }
