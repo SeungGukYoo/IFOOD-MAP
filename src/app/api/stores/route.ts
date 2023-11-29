8a624da5db9ae343e6fee371eb16d1ba029f5914
SeungGukYoo <dbtmd878@gmail.com>
2023-11-29 22:10:31 +0900

    feat: api 분기 처리

--- src/app/api/stores/route.ts
+++ src/app/api/stores/route.ts
@@ -3,11 +3,17 @@ import { NextResponse } from "next/dist/server/web/spec-extension/response";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const query = parseInt(req.nextUrl.searchParams.get("page") || "1");
-  const skipPage = (query - 1) * 20;
   const prisma = new PrismaClient();
-  const dataSize = await prisma.store.count();
-  const stores = await prisma.store.findMany({ orderBy: { id: "asc" }, take: 20, skip: skipPage });
+  console.log(req.nextUrl.searchParams.get("page"), "abcsdafsd");
+  if (req.nextUrl.searchParams.get("page")) {
+    const query = parseInt(req.nextUrl.searchParams.get("page")!);
+    const skipPage = (query - 1) * 20;
+    const dataSize = await prisma.store.count();
+    const stores = await prisma.store.findMany({ orderBy: { id: "asc" }, take: 20, skip: skipPage });
 
-  return NextResponse.json({ data: stores, dataSize, page: query, totalPage: Math.ceil(dataSize / 20) });
+    return NextResponse.json({ data: stores, dataSize, page: query, totalPage: Math.ceil(dataSize / 20) });
+  } else {
+    const stores = await prisma.store.findMany({ orderBy: { id: "asc" } });
+    return NextResponse.json({ data: stores });
+  }
 }
