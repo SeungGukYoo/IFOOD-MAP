1d26702cf5be02e391f4460ba96f65b834bfab9e
SeungGukYoo <dbtmd878@gmail.com>
2023-11-29 16:10:14 +0900

    feat: 쿼리 값을 바탕으로 특정 범위 데이터 반환하는 함수로 변경

--- src/app/api/stores/route.ts
+++ src/app/api/stores/route.ts
@@ -1,8 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/dist/server/web/spec-extension/response";
+import { NextRequest } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const query = parseInt(req.nextUrl.searchParams.get("page") || "1");
+  const skipPage = (query - 1) * 20;
   const prisma = new PrismaClient();
-  const stores = await prisma.store.findMany({ orderBy: { id: "asc" } });
-  return NextResponse.json({ data: stores });
+  const dataSize = await prisma.store.count();
+  const stores = await prisma.store.findMany({ orderBy: { id: "asc" }, take: 20, skip: skipPage });
+
+  return NextResponse.json({ data: stores, dataSize, page: query, totalPage: Math.ceil(dataSize / 20) });
 }
