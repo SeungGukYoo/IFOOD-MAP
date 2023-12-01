ab3ecba3b20d86f85b931a3320930b057ddef617
SeungGukYoo <dbtmd878@gmail.com>
2023-12-01 13:59:34 +0900

    feat: 숫자 또는 문자 타입으로 확장

--- src/app/lib/getStorePageData.ts
+++ src/app/lib/getStorePageData.ts
@@ -6,9 +6,8 @@ interface StoreListType {
   page: number;
   totalPage: number;
 }
-const getStorePageData: (page: string) => Promise<StoreListType> = async (page: string) => {
+const getStorePageData: (page: string | number) => Promise<StoreListType> = async (page) => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/stores?page=${page}`);
-
   if (!response.ok) {
     throw new Error("예기치 못한 에러가 발생하였습니다(getStorePageData)");
   }
