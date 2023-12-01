c48f98c60679c9d61724b9763ea6d55e8ff1e4c3
SeungGukYoo <dbtmd878@gmail.com>
2023-12-01 14:13:00 +0900

    feat: Intersection API, useInfiniteQuery를 사용한 무한 스크롤 기능 구현

--- src/hooks/useStoresList.tsx
+++ src/hooks/useStoresList.tsx
@@ -1,20 +1,48 @@
 "use client";
 
 import getStorePageData from "@/app/lib/getStorePageData";
-import { useSearchParams } from "next/navigation";
-import { useState } from "react";
-
-import { useQuery } from "react-query";
+import { useEffect, useRef } from "react";
+import { useInfiniteQuery } from "react-query";
 
 const useStoresList = () => {
-  const params = useSearchParams();
-  const query = params.get("page") || "1";
-  const { isLoading, isSuccess, isError, data } = useQuery(`stores-${query}`, () => getStorePageData(query), {
-    staleTime: 1800000,
-    refetchOnWindowFocus: false,
-  });
+  const observeTarget = useRef<null | HTMLDivElement>(null);
+  const topPos = useRef<HTMLDivElement | null>(null);
+  const { data, isLoading, isFetching, isSuccess, isError, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    `stores`,
+    ({ pageParam = 1 }) => getStorePageData(pageParam),
+    {
+      staleTime: 1800000,
+      refetchOnWindowFocus: false,
+      getNextPageParam: (lastPage) => {
+        return lastPage.data.length > 0 ? lastPage.page + 1 : undefined;
+      },
+    }
+  );
 
-  return { isLoading, isSuccess, isError, data };
+  const moveTopPage = () => {
+    topPos.current?.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const observerOption: IntersectionObserverInit = {
+      rootMargin: "-52px 0px 0px 0px",
+    };
+    const callbackFn = ([entries]: IntersectionObserverEntry[]) => {
+      if (entries.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    };
+    const observer = new IntersectionObserver(callbackFn, observerOption);
+
+    if (observeTarget.current) {
+      observer.observe(observeTarget.current);
+    }
+
+    return () => observer.disconnect();
+  }, [fetchNextPage, hasNextPage]);
+  return { isLoading, isFetching, isSuccess, isError, data, observeTarget, topPos, fetchNextPage, moveTopPage };
 };
 
 export default useStoresList;
