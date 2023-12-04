813041a3282ee059cd593de16db5d1a1cf977f44
SeungGukYoo <dbtmd878@gmail.com>
2023-12-04 16:45:10 +0900

    feat: naver 로그인 추가 및 세션 기본 옵션 수정

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -3,9 +3,10 @@ import { PrismaClient } from "@prisma/client";
 import { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next";
 import { NextAuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import NaverProvider from 'next-auth/providers/naver';
 
 
-// get current login info (only back end)
+
 const prisma = new PrismaClient()
 export const config = {
   adapter:PrismaAdapter(prisma),
@@ -14,7 +15,16 @@ export const config = {
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID??"",
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET??"",
     }),
+    NaverProvider({
+      clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID??"",
+      clientSecret: process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET??"",
+    }),
+  
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy:'jwt'
+  }
 } satisfies NextAuthOptions
 
 export function auth(...args: [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]] | [NextApiRequest, NextApiResponse] | []) {
