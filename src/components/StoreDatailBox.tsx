907a9b11ad2dcff39727260daaedef3d1d288b8e
SeungGukYoo <dbtmd878@gmail.com>
2023-12-05 17:33:32 +0900

    feat: 리액트 쿼리 사용 및 레이아웃 구현

---
+++ src/components/StoreDatailBox.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import getQueryClient from "@/app/lib/getQueryClient";
+import { getStoreData } from "@/app/lib/getStoreData";
+import { QueryCache, QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import React from "react";
+import ErrorBox from "./ErrorBox";
+
+const StoreDatailBox = ({ params }: { params: string }) => {
+  const {
+    data: store,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useQuery({
+    queryKey: [params],
+    queryFn: () => getStoreData(params),
+  });
+
+  // console.log(useQueryClient().getQueryData([params]));
+  // console.log(useQueryClient().getQueryCache());
+  // console.log(store, "this is conponent client");
+
+  if (isError) {
+    return <ErrorBox />;
+  }
+  return (
+    <div className="max-w-[1024px] mx-auto py-10">
+      <div className="px-4">
+        <h1 className="text-base font-semibold leading-3 text-gray-900 md:text-lg md:leading-[1]">상세보기</h1>
+        <p className="mt-1 text-sm leading-6 text-gray-500">식당에 대해 자세히 알아보세요.</p>
+      </div>
+      {isSuccess && (
+        <div className="border-t border-gray-100 mt-6">
+          <dl className="divide-y divide-gray-100">
+            <div className="py-6 px-4 grid grid-cols-3 gap-4 md:text-lg">
+              <dt className="font-medium leading-6 text-gray-900">가게명</dt>
+
+              <dd className="leading-6 text-gray-700 col-span-2 flex items-center"></dd>
+            </div>
+            <div className="py-6 px-4 grid grid-cols-3 gap-4 md:text-lg">
+              <dt className="font-medium leading-6 text-gray-900">번호</dt>
+
+              <dd className="leading-6 text-gray-700 col-span-2 flex items-center">{"-"}</dd>
+            </div>
+            <div className="py-6 px-4 grid grid-cols-3 gap-4 md:text-lg">
+              <dt className="font-medium leading-6 text-gray-900">주소</dt>
+
+              <dd className="leading-6 text-gray-700 col-span-2 flex items-center">{}</dd>
+            </div>
+            <div className="py-6 px-4 grid grid-cols-3 gap-4 md:text-lg">
+              <dt className="font-medium leading-6 text-gray-900">음식</dt>
+
+              <dd className="leading-6 text-gray-700 col-span-2 flex items-center">{}</dd>
+            </div>
+            <div className="py-6 px-4 grid grid-cols-3 gap-4 md:text-lg">
+              <dt className="font-medium leading-6 text-gray-900">특징</dt>
+
+              <dd className="leading-6 text-gray-700 col-span-2 flex items-center">{}</dd>
+            </div>
+            <div className="py-6 px-4 grid grid-cols-3 gap-4 md:text-lg">
+              <dt className="font-medium leading-6 text-gray-900">업소 종류</dt>
+
+              <dd className="leading-6 text-gray-700 col-span-2 flex items-center">{}</dd>
+            </div>
+          </dl>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default StoreDatailBox;
