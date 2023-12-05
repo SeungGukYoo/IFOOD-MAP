8a3277ec67a2c645fcab601da74c276bf9346324
SeungGukYoo <dbtmd878@gmail.com>
2023-12-05 13:35:12 +0900

    feat: 버튼 로그인 기능 추가

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import signInHandler from "@/util/signInHandler";
+import { signIn } from "next-auth/react";
 import { RiKakaoTalkFill } from "react-icons/ri";
 import { SiNaver } from "react-icons/si";
 
@@ -10,7 +12,10 @@ const LoginPage = () => {
         <p className="text-sm text-gray-500 mb-3 md:text-xl">음식에 진심인 그대들을 위한 지도</p>
       </div>
       <p className="text-xl">SNS 계정을 통해 쉽게 로그인하세요</p>
-      <button className="border rounded-md font-bold min-w-[350px] min-h-[50px] bg-white shadow-md text-lg ">
+      <button
+        onClick={() => signInHandler("google")}
+        className="border rounded-md font-bold min-w-[350px] min-h-[50px] bg-white shadow-md text-lg "
+      >
         <span className="text-blue-500">G</span>
         <span className="text-red-400">o</span>
         <span className="text-yellow-400">o</span>
@@ -19,11 +24,17 @@ const LoginPage = () => {
         <span className="text-blue-500">e</span>
         <span className="ml-2">구글 로그인</span>
       </button>
-      <button className="border rounded-md font-bold min-w-[350px] min-h-[50px] bg-yellow-400 shadow-md text-lg flex items-center justify-center gap-2">
+      <button
+        onClick={() => signInHandler("kakao")}
+        className="border rounded-md font-bold min-w-[350px] min-h-[50px] bg-yellow-400 shadow-md text-lg flex items-center justify-center gap-2"
+      >
         <RiKakaoTalkFill className="text-2xl" />
         카카오 로그인
       </button>
-      <button className="border rounded-md font-bold min-w-[350px] min-h-[50px] bg-green-500 shadow-md text-white text-lg flex items-center justify-center gap-2">
+      <button
+        onClick={() => signInHandler("naver")}
+        className="border rounded-md font-bold min-w-[350px] min-h-[50px] bg-green-500 shadow-md text-white text-lg flex items-center justify-center gap-2"
+      >
         <SiNaver className="text-xl" />
         네이버 로그인
       </button>
