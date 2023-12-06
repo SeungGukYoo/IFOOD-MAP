456f70b960e8dd6131048c98eb9b26bab148089b
SeungGukYoo <dbtmd878@gmail.com>
2023-12-06 14:26:20 +0900

    feat: 위도, 경도,높이 값을 동적으로 설정

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,27 @@
 "use client";
 import { StoresType } from "@/app/page";
 import useMap from "@/hooks/useMap";
+import { usePathname, useSearchParams } from "next/navigation";
 import Script from "next/script";
 import React, { useEffect } from "react";
 import StoreBox from "./StoreBox";
+export type PosType = string | number | undefined;
+interface MapProps {
+  store: StoresType;
+  lat: PosType;
+  lng: PosType;
+}
+const DEFAULT_LAT = 37.497625203;
+const DEFAULT_LNG = 127.03088379;
+const Map = ({ store, lat = DEFAULT_LAT, lng = DEFAULT_LNG }: MapProps) => {
+  let path = usePathname();
 
-const Map = ({ store }: { store: StoresType }) => {
-  const { currentSotre, setStoreData, loadKakaoMap, setCurrentSotre } = useMap();
+  if (typeof lat === "string" && typeof lng === "string") {
+    lat = parseFloat(lat);
+    lng = parseFloat(lng);
+  }
+
+  const { currentSotre, setStoreData, loadKakaoMap, setCurrentSotre } = useMap(lat as number, lng as number);
 
   useEffect(() => {
     if (store) {
@@ -16,7 +31,7 @@ const Map = ({ store }: { store: StoresType }) => {
 
   return (
     <div>
-      <div id="map" className="w-full h-[calc(100vh-52px)]"></div>
+      <div id="map" className={`w-full ${path === "/" ? "h-[calc(100vh-52px)]" : "h-[600px]"}`}></div>
       <Script
         strategy="afterInteractive"
         type="text/javascript"
