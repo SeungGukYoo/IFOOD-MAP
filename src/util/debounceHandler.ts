f172aa2fbb7f155fd57d46007533632d89a72d8f
SeungGukYoo <dbtmd878@gmail.com>
2023-12-07 16:17:14 +0900

    feat:  타입 수정 및 파라미터 변경

--- src/util/debounceHandler.ts
+++ src/util/debounceHandler.ts
@@ -1,8 +1,10 @@
+import { ChangeEvent } from "react";
+
 let debounceTimer: ReturnType<typeof setTimeout>;
-const debounceHandler = (callback: (value: string) => void, target: string) => {
+const debounceHandler = (callback: (value: string) => void, e: ChangeEvent<HTMLInputElement>) => {
   if (debounceTimer) clearTimeout(debounceTimer);
   debounceTimer = setTimeout(() => {
-    callback.call(this, target);
+    callback.call(this, e.target.value);
   }, 300);
   return () => clearTimeout(debounceTimer);
 };
