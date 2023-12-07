77e4545f41dadcdb494099d296979a1b61e08536
SeungGukYoo <dbtmd878@gmail.com>
2023-12-07 14:07:59 +0900

    feat: 리액트 쿼리값에 지역과 가게명 추가 및 onChange이벤트 추가

--- src/hooks/useStoresList.tsx
+++ src/hooks/useStoresList.tsx
@@ -1,25 +1,34 @@
 "use client";
 
 import getStoresPageData from "@/app/lib/getStorePageData";
+import debounceHandler from "@/util/debounceHandler";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { useEffect, useRef } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 const useStoresList = () => {
   const observeTarget = useRef<null | HTMLDivElement>(null);
   const topPos = useRef<HTMLDivElement | null>(null);
+  const [district, setDistrict] = useState<string>("");
+  const [storeName, setStoreName] = useState<string>("");
   const { data, isLoading, isFetching, isSuccess, isError, fetchNextPage, hasNextPage } = useInfiniteQuery({
-    queryKey: ["stores"],
-    queryFn: ({ pageParam }) => getStoresPageData(pageParam),
+    queryKey: ["stores", district, storeName],
+    queryFn: ({ pageParam }) => getStoresPageData(pageParam, district, storeName),
     initialPageParam: 1,
     getNextPageParam: (lastPage) => {
       return lastPage.data.length > 0 ? lastPage.page + 1 : undefined;
     },
+    staleTime: 60 * 60 * 3,
   });
 
   const moveTopPage = () => {
     topPos.current?.scrollIntoView({
       behavior: "smooth",
     });
+  };
+
+  const onChangeStoreName = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    debounceHandler(setStoreName, value);
   };
 
   useEffect(() => {
@@ -39,7 +48,19 @@ const useStoresList = () => {
 
     return () => observer.disconnect();
   }, [fetchNextPage, hasNextPage]);
-  return { isLoading, isFetching, isSuccess, isError, data, observeTarget, topPos, fetchNextPage, moveTopPage };
+  return {
+    isLoading,
+    isFetching,
+    isSuccess,
+    isError,
+    data,
+    observeTarget,
+    topPos,
+    fetchNextPage,
+    moveTopPage,
+    setDistrict,
+    onChangeStoreName,
+  };
 };
 
 export default useStoresList;
