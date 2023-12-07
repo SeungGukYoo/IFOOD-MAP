c8c9a32e9a79b7b951fa14ee7a57efc6d9fa4935
SeungGukYoo <dbtmd878@gmail.com>
2023-12-07 15:18:12 +0900

    feat: zustand 전역 상태 및 커스텀훅 추가

---
+++ src/hooks/useLocation.tsx
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+
+interface Store {
+  latitude: number;
+  longitude: number;
+}
+interface Action {
+  changeCoordinates: (lat: number, lon: number) => void;
+}
+const useLocation = create<Store & Action>()((set) => ({
+  latitude: 37.497625203,
+  longitude: 127.03088379,
+  changeCoordinates: (lat, lon) =>
+    set({
+      latitude: lat,
+      longitude: lon,
+    }),
+}));
+export default useLocation;
