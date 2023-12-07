79f271dd6f86a5f3b11697fef54dce6b188c5348
SeungGukYoo <dbtmd878@gmail.com>
2023-12-07 14:06:24 +0900

    feat: 쿼리값에 지역과 가게명 추가

--- src/app/lib/getStorePageData.ts
+++ src/app/lib/getStorePageData.ts
@@ -6,8 +6,11 @@ interface StoreListType {
   page: number;
   totalPage: number;
 }
-const getStoresPageData: (page: string | number) => Promise<StoreListType> = async (page) => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/stores?page=${page}`);
+type GetStoresData = (page: string | number, district: string, storename: string) => Promise<StoreListType>;
+const getStoresPageData: GetStoresData = async (page, district, storeName) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/stores?page=${page}&district=${district}&store=${storeName}`
+  );
   if (!response.ok) {
     throw new Error("예기치 못한 에러가 발생하였습니다(getStorePageData)");
   }
