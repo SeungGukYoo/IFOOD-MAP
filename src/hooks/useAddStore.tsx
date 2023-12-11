9ce35697832930338ce9657d2d799fb10460e02d
SeungGukYoo <dbtmd878@gmail.com>
2023-12-12 01:35:11 +0900

    feat: 위치 기반 위도 경도 호출 Rest API 추가

--- src/hooks/useAddStore.tsx
+++ src/hooks/useAddStore.tsx
@@ -2,12 +2,14 @@ import { StoreType } from "@/app/page";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import usePostcode from "./usePostcode";
 
 type Inputs = {
   name: string;
   category: string;
   phone: string;
-  address: string;
+  roadAddress: string;
+  postAddress: string;
   foodCertifyName: string;
   storeType: string;
 };
@@ -16,20 +18,35 @@ const useAddStore = () => {
   const {
     register,
     handleSubmit,
+    watch,
+    setValue,
     formState: { errors },
   } = useForm<Inputs>();
   const router = useRouter();
+  watch("name");
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     try {
-      const response = await fetch("/api/store", {
-        method: "POST",
-        body: JSON.stringify({ data: data }),
-      });
-      if (response.status !== 201) {
-        throw new Error("예기치 못한 에러가 발생하였습니다.");
-      }
-      const json: StoreType = await response.json();
-      router.replace(`/stores/${json.id}`);
+      const positionResponse = await fetch(
+        `https://dapi.kakao.com/v2/local/search/address.JSON?query=${data.roadAddress}`,
+        {
+          method: "GET",
+          headers: {
+            "Authorization": `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_RESTAPI_KEY}`,
+            "content-type": "application/json;charset=UTF-8",
+          },
+        }
+      );
+      const positionJson = await positionResponse.json();
+
+      // const response = await fetch("/api/store", {
+      //   method: "POST",
+      //   body: JSON.stringify({ data: data }),
+      // });
+      // if (response.status !== 201) {
+      //   throw new Error("예기치 못한 에러가 발생하였습니다.");
+      // }
+      // const json: StoreType = await response.json();
+      // router.replace(`/stores/${json.id}`);
     } catch (error) {
       console.error(error);
     }
