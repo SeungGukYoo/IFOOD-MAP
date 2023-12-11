1c621f7e348ac8a413b4e455ee44d75adc19e989
SeungGukYoo <dbtmd878@gmail.com>
2023-12-12 01:32:45 +0900

    feat: Script 코드 제거

--- src/app/stores/new/page.tsx
+++ src/app/stores/new/page.tsx
@@ -1,23 +1,11 @@
 "use client";
 import FormBox from "@/components/FormBox";
-import Script from "next/script";
 
 import React from "react";
 
 const NewPage = () => {
-  const complete = () => {
-    console.log("complete1");
-  };
-
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        type="text/javascript"
-        src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
-        onReady={complete}
-      />
-
       <FormBox />
     </>
   );
