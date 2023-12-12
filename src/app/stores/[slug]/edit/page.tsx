8775acca9b42e0566e42336b4c030269fb7d0d3d
SeungGukYoo <dbtmd878@gmail.com>
2023-12-12 17:25:40 +0900

    feat: 수정페이지 폼 컴포넌트 연결

--- src/app/stores/[slug]/edit/page.tsx
+++ src/app/stores/[slug]/edit/page.tsx
@@ -1,7 +1,14 @@
+import { getStoreData } from "@/app/lib/getStoreData";
+import FormBox from "@/components/FormBox";
 import React from "react";
 
-const EditPage = () => {
-  return <div>edit page</div>;
+const EditPage = async ({ params }: { params: { slug: string } }) => {
+  const data = await getStoreData(params.slug);
+  return (
+    <>
+      <FormBox storeData={data} />
+    </>
+  );
 };
 
 export default EditPage;
