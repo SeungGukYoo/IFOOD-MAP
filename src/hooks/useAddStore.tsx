c7774e8dffe1234c7b31d422c8afc8dafc70f9d9
SeungGukYoo <dbtmd878@gmail.com>
2023-12-12 14:55:27 +0900

    feat: 저장후 페이지 이동, 스토어 초기화 및 코드 리팩토링

--- src/hooks/useAddStore.tsx
+++ src/hooks/useAddStore.tsx
@@ -1,56 +1,96 @@
-import { StoreType } from "@/app/page";
+import { setStoreData } from "@/app/lib/setStoreData";
 import { useRouter } from "next/navigation";
-import React from "react";
+import { useEffect } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import usePostcode from "./usePostcode";
+import useAddress from "./useAddressStore";
+import useKakaoClientStore from "./useKakaoClientStore";
+import useLocationStore from "./useLocationStore";
 
-type Inputs = {
+export interface Inputs {
   name: string;
   category: string;
   phone: string;
-  roadAddress: string;
-  postAddress: string;
+  address: string;
   foodCertifyName: string;
   storeType: string;
-};
+}
+export interface LocationDocuments {
+  address: {
+    address_name: string;
+    b_code: string;
+    h_code: string;
+    main_address_no: string;
+    mountain_yn: string;
+    region_1depth_name: string;
+    region_2depth_name: string;
+    region_3depth_h_name: string;
+    region_3depth_name: string;
+    sub_address_no: string;
+    x: string;
+    y: string;
+  };
+  address_name: string;
+  address_type: string;
+  road_address: {
+    address_name: string;
+    building_name: string;
+    main_building_no: string;
+    region_1depth_name: string;
+    region_2depth_name: string;
+    region_3depth_name: string;
+    road_name: string;
+    sub_building_no: string;
+    underground_yn: string;
+    x: string;
+    y: string;
+    zone_no: string;
+  };
+  x: string;
+  y: string;
+}
+export interface SearchJson {
+  documents: Array<LocationDocuments>;
+  meta: {
+    is_end: boolean;
+    pageable_count: number;
+    total_count: number;
+  };
+}
 
 const useAddStore = () => {
   const {
     register,
     handleSubmit,
-    watch,
     setValue,
     formState: { errors },
   } = useForm<Inputs>();
   const router = useRouter();
-  watch("name");
+  const { address, changeAddress } = useAddress();
+  const { kakaoClient } = useKakaoClientStore();
+  const { changeCoordinates } = useLocationStore();
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     try {
-      const positionResponse = await fetch(
-        `https://dapi.kakao.com/v2/local/search/address.JSON?query=${data.roadAddress}`,
-        {
-          method: "GET",
-          headers: {
-            "Authorization": `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_RESTAPI_KEY}`,
-            "content-type": "application/json;charset=UTF-8",
-          },
-        }
-      );
-      const positionJson = await positionResponse.json();
-
-      // const response = await fetch("/api/store", {
-      //   method: "POST",
-      //   body: JSON.stringify({ data: data }),
-      // });
-      // if (response.status !== 201) {
-      //   throw new Error("예기치 못한 에러가 발생하였습니다.");
-      // }
-      // const json: StoreType = await response.json();
-      // router.replace(`/stores/${json.id}`);
+      const locationResult = await kakaoClient.locationSearch(data.address);
+      const { x, y } = locationResult;
+      const includeLocationData = {
+        ...data,
+        lat: y,
+        lng: x,
+      };
+      const response = await setStoreData(includeLocationData);
+      changeCoordinates(parseFloat(y), parseFloat(x));
+      changeAddress("");
+      router.replace(`/stores/${response?.id}`);
     } catch (error) {
       console.error(error);
     }
   };
+
+  useEffect(() => {
+    if (address) {
+      setValue("address", address);
+    }
+  }, [address, setValue]);
 
   return {
     register,
