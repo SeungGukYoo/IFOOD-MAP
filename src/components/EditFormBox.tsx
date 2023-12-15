f7eecd3e5aeb97fd3bc970d26f90f933bf56d26a
SeungGukYoo <dbtmd878@gmail.com>
2023-12-15 17:06:25 +0900

    feat: 캐시값 재요청을 위한 캐시 무효화 로직 추가

--- src/components/EditFormBox.tsx
+++ src/components/EditFormBox.tsx
@@ -3,12 +3,13 @@ import { getStoreData } from "@/app/lib/getStoreData";
 import { setStoreData } from "@/app/lib/setStoreData";
 import { StoreType } from "@/app/page";
 import useAddress from "@/hooks/useAddressStore";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import React from "react";
 import FormBox from "./FormBox";
 
 const EditFormBox = ({ id }: { id: string }) => {
+  const queryClient = useQueryClient();
   const router = useRouter();
   const { changeAddress } = useAddress();
   const { data, isLoading, isSuccess } = useQuery({
@@ -21,9 +22,10 @@ const EditFormBox = ({ id }: { id: string }) => {
     mutationKey: ["store", id],
     mutationFn: async (data: StoreType) => {
       const response = await setStoreData({ ...data, id: parseInt(id) });
+      queryClient.invalidateQueries({ queryKey: ["store", id] });
       return response;
     },
-    onSuccess: (data) => {
+    onSuccess: () => {
       changeAddress("");
       router.replace(`/stores/${id}`);
       router.refresh();
