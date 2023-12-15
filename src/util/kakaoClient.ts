9c7569be9d4082bc1b14a14ab8313d06f9a48dd5
SeungGukYoo <dbtmd878@gmail.com>
2023-12-15 15:47:00 +0900

    chore: 타입 경로 수정

--- src/util/kakaoClient.ts
+++ src/util/kakaoClient.ts
@@ -1,5 +1,3 @@
-import type { LocationDocuments, SearchJson } from "@/hooks/useAddStore";
-
 interface IKakaoClient {
   url: string;
   locationSearch(data: string): Promise<LocationDocuments>;
@@ -29,3 +27,46 @@ class KakaoClient implements IKakaoClient {
   }
 }
 export default KakaoClient;
+
+export interface LocationDocuments {
+  address: {
+    address_name: string;
+    b_code: string;
+    h_code: string;
+    main_address_no: string;
+    mountain_yn: string;
+    region_1depth_name: string;
+    region_2depth_name: string;
+    region_3depth_h_name: string;
+    region_3depth_name: string;
+    sub_address_no: string;
+    x: string;
+    y: string;
+  };
+  address_name: string;
+  address_type: string;
+  road_address: {
+    address_name: string;
+    building_name: string;
+    main_building_no: string;
+    region_1depth_name: string;
+    region_2depth_name: string;
+    region_3depth_name: string;
+    road_name: string;
+    sub_building_no: string;
+    underground_yn: string;
+    x: string;
+    y: string;
+    zone_no: string;
+  };
+  x: string;
+  y: string;
+}
+export interface SearchJson {
+  documents: Array<LocationDocuments>;
+  meta: {
+    is_end: boolean;
+    pageable_count: number;
+    total_count: number;
+  };
+}
