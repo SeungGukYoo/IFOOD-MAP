68eae711b892f22c9f9665101a96c5868a7e5a06
SeungGukYoo <dbtmd878@gmail.com>
2023-12-18 14:03:53 +0900

    feat: 데이터 삭제 mutation 구현

---
+++ src/hooks/useDeleteStore.ts
@@ -0,0 +1,25 @@
+import deleteStoreData from "@/app/lib/deleteStoreData";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+
+const useDeleteStore = (id: string | string[]) => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: () => deleteStoreData(id),
+    mutationKey: ["store", id],
+    onSuccess: (data) => {
+      if (data.ok) {
+        queryClient.removeQueries({ queryKey: ["store", id] });
+        router.replace("/stores");
+      }
+    },
+    onError: (err) => {
+      console.log(err);
+    },
+  });
+
+  return mutation;
+};
+
+export default useDeleteStore;
