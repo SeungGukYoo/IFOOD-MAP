da5a0a5a5df2dda9aab455f71f9e1a676c314867
SeungGukYoo <dbtmd878@gmail.com>
2023-12-19 23:39:48 +0900

    style: 불필요한 코드 제거

--- src/hooks/useStoreBox.tsx
+++ src/hooks/useStoreBox.tsx
@@ -1,17 +1,15 @@
 import { StoreType } from "@/app/page";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 import useLocationStore from "./useLocationStore";
 
 const useStoreBox = (store: StoreType) => {
-  const [like, setLike] = useState(false);
   const { changeCoordinates } = useLocationStore();
   const router = useRouter();
   const moveDetailPage = () => {
     changeCoordinates(parseFloat(store.lat!), parseFloat(store.lng!));
     router.push(`/stores/${store.id}`);
   };
-  return { like, setLike, moveDetailPage };
+  return { moveDetailPage };
 };
 
 export default useStoreBox;
