e3a6a6583e0b33d3f9beceb667dbb686c15de231
SeungGukYoo <dbtmd878@gmail.com>
2023-12-19 23:44:00 +0900

    feat: 단일 데이터 호출시 like 데이터 포함 및 존재하지 않는 경우의 반환값 수정

--- src/app/api/store/route.ts
+++ src/app/api/store/route.ts
@@ -1,18 +1,29 @@
 import { StoreType } from "@/app/page";
+import { auth } from "@/util/auth";
 import prisma from "@/util/prismaClient";
 
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const id = req.nextUrl.searchParams.get("id");
+  const autoInfo = await auth();
+
   if (!id) {
-    return NextResponse.error();
+    return NextResponse.json({ message: "존재하지 않는 정보입니다." }, { status: 404 });
   }
   const data = await prisma.store.findUnique({
     where: {
       id: parseInt(id),
     },
+    include: {
+      likes: {
+        where: autoInfo?.user.access_token?.sub
+          ? { userId: parseInt(autoInfo?.user.access_token?.sub), storeId: parseInt(id) }
+          : {},
+      },
+    },
   });
+
   return NextResponse.json(data, { status: 200 });
 }
 
@@ -24,7 +35,7 @@ export async function POST(req: Request) {
   return NextResponse.json(store, { status: 201 });
 }
 export async function PATCH(req: Request) {
-  const { data }: { data: StoreType } = await req.json();
+  const { data } = await req.json();
   const store = await prisma.store.update({
     where: {
       id: data.id,
