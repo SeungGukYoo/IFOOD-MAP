e60e903928f8d66872c4602abd53f98a0b743773
SeungGukYoo <dbtmd878@gmail.com>
2023-12-19 23:35:16 +0900

    feat: 좋아요 Post,Get 호출 함수 구현

---
+++ src/app/lib/setLikeData.ts
@@ -0,0 +1,13 @@
+export async function setLikeData(id: string) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/like`, {
+    method: "POST",
+    body: JSON.stringify({
+      storeId: id,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("setLikeData에럭 발생");
+  }
+  return response.json();
+}
