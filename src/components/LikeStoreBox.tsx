75223dc2edc1a0ea96c3e3c0d30da23bd683d6ed
SeungGukYoo <dbtmd878@gmail.com>
2023-12-20 16:17:45 +0900

    feat: 좋아요 업데이트 기능 커스텀 훅으로 분리

--- src/components/LikeStoreBox.tsx
+++ src/components/LikeStoreBox.tsx
@@ -1,29 +1,15 @@
-import { getLikeData } from "@/app/lib/getLikeData";
-import { setLikeData } from "@/app/lib/setLikeData";
 import { StoreType } from "@/app/page";
-import React, { useState } from "react";
+import useLikeStore from "@/hooks/useLikeStore";
+import React from "react";
 import { CiHeart } from "react-icons/ci";
 import { FcLike } from "react-icons/fc";
-import LikeButton from "./LikeButton";
 
 const LikeStoreBox = ({ store }: { store: StoreType }) => {
-  const [like, setLike] = useState(!!store.likes.length);
-  const handleLike = async () => {
-    try {
-      const data = await setLikeData(store.id?.toString()!);
-
-      setLike((prev) => (prev = !prev));
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  const getLike = async () => {
-    const data = await getLikeData(store.id!);
-  };
+  const { like, updateLike } = useLikeStore(store);
 
   return (
     <>
-      <button onClick={handleLike} className="text-xl">
+      <button onClick={() => updateLike(store.id)} className="text-xl">
         {like ? <FcLike /> : <CiHeart className="text-gray-400" />}
       </button>
     </>
