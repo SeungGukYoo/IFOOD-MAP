b67ee265c246920e149e45daa2fab5aa95628211
SeungGukYoo <dbtmd878@gmail.com>
2023-12-21 14:25:14 +0900

    feat: 가게 삭제시 캐시 삭제 및 스토어 재호출

--- src/hooks/useDeleteStore.ts
+++ src/hooks/useDeleteStore.ts
@@ -1,17 +1,28 @@
 import deleteStoreData from "@/app/lib/deleteStoreData";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import useSearchStore from "./useSearchStore";
 
 const useDeleteStore = (id: string | string[]) => {
   const router = useRouter();
   const queryClient = useQueryClient();
+  const { district, name, changeDistrict, changeName } = useSearchStore();
   const mutation = useMutation({
-    mutationFn: () => deleteStoreData(id),
+    mutationFn: async () => {
+      const response = await deleteStoreData(id);
+      if (response.ok) {
+        queryClient.removeQueries({ queryKey: ["store", id] });
+        queryClient.removeQueries({ queryKey: ["stores", district, name] });
+      }
+      return response;
+    },
     mutationKey: ["store", id],
     onSuccess: (data) => {
       if (data.ok) {
-        queryClient.removeQueries({ queryKey: ["store", id] });
+        changeName("");
+        changeDistrict("");
         router.replace("/stores");
+        router.refresh();
       }
     },
     onError: (err) => {
