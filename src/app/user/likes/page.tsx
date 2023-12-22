ea8544bb31f4e64b144c16f6cacc0a61411d25f4
SeungGukYoo <dbtmd878@gmail.com>
2023-12-22 15:06:34 +0900

    feat: 좋아요 페이지 SSR 및 하이드레이션 적용

--- src/app/user/likes/page.tsx
+++ src/app/user/likes/page.tsx
@@ -1,7 +1,24 @@
+import { getLikesData } from "@/app/lib/getLikesData";
+import getQueryClient from "@/app/lib/getQueryClient";
+import LikesStoreList from "@/components/LikesStoreList";
+import { auth } from "@/util/auth";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import React from "react";
 
-const LikesPage = () => {
-  return <div>likes page</div>;
+const LikesPage = async () => {
+  const authInfo = await auth();
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["stores", "likes", authInfo?.user.access_token?.sub],
+    queryFn: () => getLikesData(authInfo?.user.access_token?.sub!),
+  });
+  const dehydrateState = dehydrate(queryClient);
+  
+  return (
+    <HydrationBoundary state={dehydrateState}>
+      <LikesStoreList id={authInfo?.user.access_token?.sub!} />
+    </HydrationBoundary>
+  );
 };
 
 export default LikesPage;
