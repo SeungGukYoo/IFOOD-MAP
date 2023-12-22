dd6a0e4df7e155eb1fba2fecd692ea22eecc4916
SeungGukYoo <dbtmd878@gmail.com>
2023-12-22 15:06:02 +0900

    feat: 좋아요 페이지 레이아웃 컴포넌트 추가

---
+++ src/app/user/likes/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from "react";
+
+const layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="max-w-[1024px] mx-auto mt-5 px-3">
+      <h2 className="text-base font-semibold leading-7 text-gray-900 md:text-xl">찜한 가게</h2>
+      <p className="mt-1 text-sm leading-6 text-gray-600 md:text-base">잊어버리지 않게 찜한 가게를 모아보세요!</p>
+      <div className="mt-4">{children}</div>
+    </div>
+  );
+};
+
+export default layout;
