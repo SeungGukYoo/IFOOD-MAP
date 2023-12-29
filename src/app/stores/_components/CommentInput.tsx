5bbb512217734b4f663fff04d7260be6161ab98f
SeungGukYoo <dbtmd878@gmail.com>
2023-12-29 15:08:03 +0900

    feat: 데이터 수정 로직 구현

--- src/app/stores/_components/CommentInput.tsx
+++ src/app/stores/_components/CommentInput.tsx
@@ -1,5 +1,7 @@
+import setCommentData from "@/app/lib/setCommentData";
 import { Comment } from "@/app/page";
 import useCommentEditStore from "@/hooks/useCommentEditStore";
+import useEditComment from "@/hooks/useEditComment";
 import usePopupStore from "@/hooks/usePopupStore";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import React, { useEffect } from "react";
@@ -7,17 +9,19 @@ import React, { useEffect } from "react";
 const CommentInput = ({ comment, idx }: { comment: Comment; idx: number }) => {
   const queryClient = useQueryClient();
   const { mutate } = useMutation({
-    // mutationFn: (content: string) => setCommentData(, comment.id),
     mutationKey: ["store", comment.storeId.toString()],
-    // onSuccess: (data) => {
-    // if (data?.ok) {
-    //   queryClient.invalidateQueries({ queryKey: ["store", comment.storeId.toString()] });
-    //   setIsPopup(-1);
-    // }
-    // },
+    mutationFn: () => setCommentData(content, comment.id),
+    onSuccess: (data) => {
+      if (data?.ok) {
+        queryClient.invalidateQueries({ queryKey: ["store", comment.storeId.toString()] });
+        setIsEdit(-1);
+      }
+    },
   });
+
   const { setIsPopup } = usePopupStore();
   const { isEdit, setIsEdit } = useCommentEditStore();
+  const { content, onChangeContent } = useEditComment(comment);
   useEffect(() => {
     if (idx === isEdit) {
       setIsPopup(-1);
@@ -31,11 +35,15 @@ const CommentInput = ({ comment, idx }: { comment: Comment; idx: number }) => {
             <textarea
               rows={10}
               className="p-2 test-[16px] w-full leading-5 lg:leading-none border resize-none mt-3 rounded outline-none shadow"
-              defaultValue={comment.content}
+              onChange={onChangeContent}
+              defaultValue={content}
             />
 
             <div className="flex gap-4 justify-end mt-2">
-              <button className=" px-4 py-1 rounded-2xl right-2 bg-gray-400 text-white md:bottom-[7px] shadow">
+              <button
+                className=" px-4 py-1 rounded-2xl right-2 bg-gray-400 text-white md:bottom-[7px] shadow"
+                onClick={() => mutate()}
+              >
                 수정
               </button>
               <button
