6246f9506234bcd436323d267cb5c8b9fffc9fb4
SeungGukYoo <dbtmd878@gmail.com>
2024-01-04 15:29:58 +0900

    feat: 지도 로딩 컴포넌트 연결

--- src/app/stores/_components/Map.tsx
+++ src/app/stores/_components/Map.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import LocationButton from "@/app/_components/LocationButton";
+import MapLoadingSpiner from "@/app/_components/MapLoadingSpiner";
 import { StoresType } from "@/app/page";
+import useGlobalLoadingStore from "@/hooks/useGlobalLoadingStore";
 import useMap from "@/hooks/useMap";
 import { usePathname } from "next/navigation";
 import Script from "next/script";
@@ -15,6 +17,7 @@ interface MapProps {
 const Map = ({ store }: MapProps) => {
   const { currentSotre, setStoreData, loadKakaoMap, setCurrentSotre } = useMap();
   const path = usePathname();
+  const { isLoading } = useGlobalLoadingStore();
 
   useEffect(() => {
     if (store) {
@@ -24,13 +27,15 @@ const Map = ({ store }: MapProps) => {
 
   return (
     <div>
-      <div id="map" className={`w-full ${path === "/" ? "h-[calc(100vh-52px)]" : "h-[600px]"}`}></div>
       <Script
         strategy="afterInteractive"
         type="text/javascript"
         src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_KEY}&autoload=false`}
         onReady={() => loadKakaoMap(store)}
       />
+      <div id="map" className={`w-full ${path === "/" ? "h-[calc(100vh-52px)]" : "h-[600px]"} relative`}></div>
+      {isLoading && <MapLoadingSpiner />}
+
       {path === "/" && <LocationButton />}
       {currentSotre && <StoreBox store={currentSotre} setCurrentSotre={setCurrentSotre} />}
     </div>
